892db3d47c3f033282a5b265ebc34411c1771789
Ricardo Vieira <ricvi.ricardovieira@gmail.com>
2020-06-29 19:04:14 +0100

    Adicionado o controller e views dos Utilizadores e EventoUtilizadores

---
+++ Mygev/Mygev/Controllers/EventoUtilizadoresController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mygev.Data;
+using Mygev.Models;
+
+namespace Mygev.Controllers
+{
+    public class EventoUtilizadoresController : Controller
+    {
+        private readonly MygevDB _context;
+
+        public EventoUtilizadoresController(MygevDB context)
+        {
+            _context = context;
+        }
+
+        // GET: EventoUtilizadores
+        public async Task<IActionResult> Index()
+        {
+            var mygevDB = _context.EventoUtilizadores.Include(e => e.Evento).Include(e => e.Utilizador);
+            return View(await mygevDB.ToListAsync());
+        }
+
+        // GET: EventoUtilizadores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventoUtilizadores = await _context.EventoUtilizadores
+                .Include(e => e.Evento)
+                .Include(e => e.Utilizador)
+                .FirstOrDefaultAsync(m => m.IDEU == id);
+            if (eventoUtilizadores == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventoUtilizadores);
+        }
+
+        // GET: EventoUtilizadores/Create
+        public IActionResult Create()
+        {
+            ViewData["IDEvento"] = new SelectList(_context.Evento, "ID", "Descricao");
+            ViewData["IDUser"] = new SelectList(_context.Utilizadores, "ID", "Email");
+            return View();
+        }
+
+        // POST: EventoUtilizadores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IDEU,IDUser,IDEvento,Permissao")] EventoUtilizadores eventoUtilizadores)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(eventoUtilizadores);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IDEvento"] = new SelectList(_context.Evento, "ID", "Descricao", eventoUtilizadores.IDEvento);
+            ViewData["IDUser"] = new SelectList(_context.Utilizadores, "ID", "Email", eventoUtilizadores.IDUser);
+            return View(eventoUtilizadores);
+        }
+
+        // GET: EventoUtilizadores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventoUtilizadores = await _context.EventoUtilizadores.FindAsync(id);
+            if (eventoUtilizadores == null)
+            {
+                return NotFound();
+            }
+            ViewData["IDEvento"] = new SelectList(_context.Evento, "ID", "Descricao", eventoUtilizadores.IDEvento);
+            ViewData["IDUser"] = new SelectList(_context.Utilizadores, "ID", "Email", eventoUtilizadores.IDUser);
+            return View(eventoUtilizadores);
+        }
+
+        // POST: EventoUtilizadores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IDEU,IDUser,IDEvento,Permissao")] EventoUtilizadores eventoUtilizadores)
+        {
+            if (id != eventoUtilizadores.IDEU)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(eventoUtilizadores);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EventoUtilizadoresExists(eventoUtilizadores.IDEU))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IDEvento"] = new SelectList(_context.Evento, "ID", "Descricao", eventoUtilizadores.IDEvento);
+            ViewData["IDUser"] = new SelectList(_context.Utilizadores, "ID", "Email", eventoUtilizadores.IDUser);
+            return View(eventoUtilizadores);
+        }
+
+        // GET: EventoUtilizadores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var eventoUtilizadores = await _context.EventoUtilizadores
+                .Include(e => e.Evento)
+                .Include(e => e.Utilizador)
+                .FirstOrDefaultAsync(m => m.IDEU == id);
+            if (eventoUtilizadores == null)
+            {
+                return NotFound();
+            }
+
+            return View(eventoUtilizadores);
+        }
+
+        // POST: EventoUtilizadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var eventoUtilizadores = await _context.EventoUtilizadores.FindAsync(id);
+            _context.EventoUtilizadores.Remove(eventoUtilizadores);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EventoUtilizadoresExists(int id)
+        {
+            return _context.EventoUtilizadores.Any(e => e.IDEU == id);
+        }
+    }
+}
---
+++ Mygev/Mygev/Controllers/UtilizadoresController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mygev.Data;
+using Mygev.Models;
+
+namespace Mygev.Controllers
+{
+    public class UtilizadoresController : Controller
+    {
+        private readonly MygevDB _context;
+
+        public UtilizadoresController(MygevDB context)
+        {
+            _context = context;
+        }
+
+        // GET: Utilizadores
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Utilizadores.ToListAsync());
+        }
+
+        // GET: Utilizadores/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilizadores = await _context.Utilizadores
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (utilizadores == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilizadores);
+        }
+
+        // GET: Utilizadores/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Utilizadores/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ID,NomeUser,Email,Password")] Utilizadores utilizadores)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(utilizadores);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilizadores);
+        }
+
+        // GET: Utilizadores/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilizadores = await _context.Utilizadores.FindAsync(id);
+            if (utilizadores == null)
+            {
+                return NotFound();
+            }
+            return View(utilizadores);
+        }
+
+        // POST: Utilizadores/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ID,NomeUser,Email,Password")] Utilizadores utilizadores)
+        {
+            if (id != utilizadores.ID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(utilizadores);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UtilizadoresExists(utilizadores.ID))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(utilizadores);
+        }
+
+        // GET: Utilizadores/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var utilizadores = await _context.Utilizadores
+                .FirstOrDefaultAsync(m => m.ID == id);
+            if (utilizadores == null)
+            {
+                return NotFound();
+            }
+
+            return View(utilizadores);
+        }
+
+        // POST: Utilizadores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var utilizadores = await _context.Utilizadores.FindAsync(id);
+            _context.Utilizadores.Remove(utilizadores);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UtilizadoresExists(int id)
+        {
+            return _context.Utilizadores.Any(e => e.ID == id);
+        }
+    }
+}
